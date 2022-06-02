30f89ad1818d58673f1ccb01b5cbc2f15f2340d2
anaes0 <anaesvg@gmail.com>
2022-06-02 16:37:42 +0100

    update script- permissions added

--- script.sh
+++ script.sh
@@ -4,14 +4,20 @@ mkdir fol_2
 
 cd fol_1
 touch 1_1.txt
+chmod 600 1_1.txt
 touch 1_2.txt
+chmod 777 1_2.txt
 touch 1_3.txt
+chmod 600 1_1.txt
 
 cd ..
 cd fol_2
 touch 2_1.txt
+chmod 600 1_1.txt
 touch 2_2.txt
+chmod 777 1_2.txt
 touch 2_3.txt
+chmod 600 1_1.txt
 
 cd ..
 rm -r fol_1/
